131158cdc64267c5db460cc5256d84024b503bfb
Sandra Laguerta <slaguerta@alumni.nd.edu>
2017-12-05 22:51:12 -0600

    Updated comic books controller's "Add", "Detail", and "Index" action methods

--- src/ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
+++ src/ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
@@ -8,6 +8,7 @@ using System.Data.Entity;
 using ComicBookLibraryManagerWebApp.ViewModels;
 using System.Net;
 using System.Data.Entity.Infrastructure;
+using ComicBookShared.Data;
 
 namespace ComicBookLibraryManagerWebApp.Controllers
 {
@@ -16,11 +17,19 @@ namespace ComicBookLibraryManagerWebApp.Controllers
     /// </summary>
     public class ComicBooksController : Controller
     {
-        public ActionResult Index()
+        private Context _context = null;
+
+        public ComicBooksController()
         {
-            // TODO Get the comic books list.
-            // Include the "Series" navigation property.
-            var comicBooks = new List<ComicBook>();
+            _context = new Context();
+        }
+        public ActionResult Index()
+        {            
+            var comicBooks = _context.ComicBooks
+                .Include(cb => cb.Series)
+                .OrderBy(cb => cb.Series.Title)
+                .ThenBy(cb => cb.IssueNumber)
+                .ToList();
 
             return View(comicBooks);
         }
@@ -31,10 +40,13 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-
-            // TODO Get the comic book.
-            // Include the "Series", "Artists.Artist", and "Artists.Role" navigation properties.
-            var comicBook = new ComicBook();
+                        
+            var comicBook = _context.ComicBooks
+                .Include(cb => cb.Series)
+                .Include(cb => cb.Artists.Select(a => a.Artist))
+                .Include(cb => cb.Artists.Select(a => a.Role))
+                .Where(cb => cb.Id == id)
+                .SingleOrDefault();
 
             if (comicBook == null)
             {
@@ -52,7 +64,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             var viewModel = new ComicBooksAddViewModel();
 
             // TODO Pass the Context class to the view model "Init" method.
-            viewModel.Init();
+            viewModel.Init(_context);
 
             return View(viewModel);
         }
@@ -73,9 +85,8 @@ namespace ComicBookLibraryManagerWebApp.Controllers
 
                 return RedirectToAction("Detail", new { id = comicBook.Id });
             }
-
-            // TODO Pass the Context class to the view model "Init" method.
-            viewModel.Init();
+                        
+            viewModel.Init(_context);
 
             return View(viewModel);
         }
@@ -99,7 +110,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             {
                 ComicBook = comicBook
             };
-            viewModel.Init();
+            viewModel.Init(_context);
 
             return View(viewModel);
         }
@@ -120,7 +131,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return RedirectToAction("Detail", new { id = comicBook.Id });
             }
 
-            viewModel.Init();
+            viewModel.Init(_context);
 
             return View(viewModel);
         }
