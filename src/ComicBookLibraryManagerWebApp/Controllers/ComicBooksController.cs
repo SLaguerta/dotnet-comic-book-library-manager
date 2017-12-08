7560783a75fdd22b0a4c721f0bcaef255d9fef14
Sandra Laguerta <slaguerta@alumni.nd.edu>
2017-12-07 22:34:06 -0600

    Implemented creates and updates in ComicBooksController class

--- src/ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
+++ src/ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
@@ -63,7 +63,6 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         {
             var viewModel = new ComicBooksAddViewModel();
 
-            // TODO Pass the Context class to the view model "Init" method.
             viewModel.Init(_context);
 
             return View(viewModel);
@@ -79,7 +78,8 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 var comicBook = viewModel.ComicBook;
                 comicBook.AddArtist(viewModel.ArtistId, viewModel.RoleId);
 
-                // TODO Add the comic book.
+                _context.ComicBooks.Add(comicBook);
+                _context.SaveChanges();
 
                 TempData["Message"] = "Your comic book was successfully added!";
 
@@ -98,8 +98,9 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            // TODO Get the comic book.
-            var comicBook = new ComicBook();
+            var comicBook = _context.ComicBooks
+                .Where(cb => cb.Id == id)
+                .SingleOrDefault();
 
             if (comicBook == null)
             {
@@ -124,7 +125,8 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             {
                 var comicBook = viewModel.ComicBook;
 
-                // TODO Update the comic book.
+                _context.Entry(comicBook).State = EntityState.Modified;
+                _context.SaveChanges();
 
                 TempData["Message"] = "Your comic book was successfully updated!";
 
@@ -172,18 +174,21 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         /// <param name="comicBook">The comic book to validate.</param>
         private void ValidateComicBook(ComicBook comicBook)
         {
-            //// If there aren't any "SeriesId" and "IssueNumber" field validation errors...
-            //if (ModelState.IsValidField("ComicBook.SeriesId") &&
-            //    ModelState.IsValidField("ComicBook.IssueNumber"))
-            //{
-            //    // Then make sure that the provided issue number is unique for the provided series.
-            //    // TODO Call method to check if the issue number is available for this comic book.
-            //    if (false)
-            //    {
-            //        ModelState.AddModelError("ComicBook.IssueNumber",
-            //            "The provided Issue Number has already been entered for the selected Series.");
-            //    }
-            //}
+            // if there aren't any "SeriesId" and "IssueNumber" field validation errors...
+            if (ModelState.IsValidField("ComicBook.SeriesId") &&
+                ModelState.IsValidField("ComicBook.IssueNumber"))
+            {
+                // then make sure that the provided issue number is unique for the provided series.
+                // todo call method to check if the issue number is available for this comic book.
+                if (_context.ComicBooks
+                    .Any(cb => cb.Id != comicBook.Id &&
+                               cb.SeriesId == comicBook.SeriesId &&
+                               cb.IssueNumber == comicBook.IssueNumber))
+                {
+                    ModelState.AddModelError("ComicBook.IssueNumber",
+                        "the provided issue number has already been entered for the selected series.");
+                }
+            }
         }
 
         private bool _disposed = false;
